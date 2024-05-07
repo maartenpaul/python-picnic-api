7e0796716d8383d7dc3e6c13eb89585be0e1de01
Thijmen <jessen.thijmen@gmail.com>
2024-05-07 19:01:18 +0200

    Some results were missing from search due to the "if sole_article_ids" condition. This commit includes items that don't have a sole article id, and just sets the sole article id to None.
    Also includes some minor reformatting and refactor (updated type hint Optional for functions that can return a None).

--- python_picnic_api/helper.py
+++ python_picnic_api/helper.py
@@ -1,5 +1,6 @@
 import json
 import re
+from typing import List, Dict, Any, Optional
 
 # prefix components:
 space = "    "
@@ -10,6 +11,9 @@ last = "└── "
 
 IMAGE_SIZES = ["small", "medium", "regular", "large", "extra-large"]
 IMAGE_BASE_URL = "https://storefront-prod.nl.picnicinternational.com/static/images"
+
+SOLE_ARTICLE_ID_PATTERN = re.compile(r"sole_article_id=([0-9]+)")
+
 
 def _tree_generator(response: list, prefix: str = ""):
     """A recursive tree generator,
@@ -38,8 +42,8 @@ def _url_generator(url: str, country_code: str, api_version: str):
     return url.format(country_code.lower(), api_version)
 
 
-def _get_category_id_from_link(category_link: str) -> str:
-    pattern = r'categories/(\d+)'
+def _get_category_id_from_link(category_link: str) -> Optional[str]:
+    pattern = r"categories/(\d+)"
     first_number = re.search(pattern, category_link)
     if first_number:
         result = str(first_number.group(1))
@@ -48,16 +52,19 @@ def _get_category_id_from_link(category_link: str) -> str:
         return None
 
 
-def _get_category_name(category_link: str, categories: list) -> str:
+def _get_category_name(category_link: str, categories: list) -> Optional[str]:
     category_id = _get_category_id_from_link(category_link)
     if category_id:
-        category = next((item for item in categories if item["id"] == category_id), None)
+        category = next(
+            (item for item in categories if item["id"] == category_id), None
+        )
         if category:
             return category["name"]
         else:
             return None
     else:
         return None
+
 
 def get_recipe_image(id: str, size="regular"):
     sizes = IMAGE_SIZES + ["1250x1250"]
@@ -66,37 +73,33 @@ def get_recipe_image(id: str, size="regular"):
 
 
 def get_image(id: str, size="regular", suffix="webp"):
-    assert "tile" in size if suffix == "webp" else True, (
-        "webp format only supports tile sizes"
-    )
+    assert (
+        "tile" in size if suffix == "webp" else True
+    ), "webp format only supports tile sizes"
     assert suffix in ["webp", "png"], "suffix must be webp or png"
     sizes = IMAGE_SIZES + [f"tile-{size}" for size in IMAGE_SIZES]
 
-    assert size in sizes, (
-        "size must be one of: " + ", ".join(sizes)
-    )
+    assert size in sizes, "size must be one of: " + ", ".join(sizes)
     return f"{IMAGE_BASE_URL}/{id}/{size}.{suffix}"
 
 
-def _extract_search_results(raw_results: dict) -> list:
+def _extract_search_results(raw_results: Dict[str, Any]) -> List[Dict[str, Any]]:
+    """Extract search results from a nested dictionary structure returned by Picnic search."""
     search_results = []
-    sole_article_id_pattern = re.compile(r"sole_article_id=([0-9]+)")
 
-    # Iterate over the nested structure of raw_results
-    for child1 in raw_results.get("body", {}).get("children", []):
-        for child2 in child1.get("children", []):
-            content = child2.get("content")
-            if content and "selling_unit" in content:
-                # Extracting the sole_article_id from the serialized JSON of pml
-                sole_article_ids = sole_article_id_pattern.findall(
-                    json.dumps(child2.get("pml", {}))
+    for section in raw_results.get("body", {}).get("children", []):
+        for item in section.get("children", []):
+            content = item.get("content", {})
+            if "selling_unit" in content:
+                sole_article_ids = SOLE_ARTICLE_ID_PATTERN.findall(
+                    json.dumps(item.get("pml", {}))
                 )
-                if sole_article_ids:
-                    sole_article_id = sole_article_ids[0]
-                    # Create and append the result entry
-                    result_entry = {
-                        **content["selling_unit"],
-                        "sole_article_id": sole_article_id,
-                    }
-                    search_results.append(result_entry)
+                sole_article_id = sole_article_ids[0] if sole_article_ids else None
+
+                result_entry = {
+                    **content["selling_unit"],
+                    "sole_article_id": sole_article_id,
+                }
+                search_results.append(result_entry)
+
     return search_results
